bc7426a97c5ae027c514cfc9053c5314ef0a7667
navisstrong <75235702+navisstrong@users.noreply.github.com>
2022-09-23 13:05:33 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket"
-    key = "main"
-    region = "us-east-2"
+    bucket = "cf-templates-39wte8okibln-ap-south-1"
+    key = "sample"
+    region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
   }
 }
